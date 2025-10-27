35abb5c149b543dda6fcaca1533fc13a802abf61
Pankajshahare <110549683+pnkjshahare@users.noreply.github.com>
2025-10-27 14:38:47 +0530

    Correct loop and add variable initialization
    Fix loop condition and variable declaration issues.

--- Hello.cpp
+++ Hello.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 int main()
 {
+    int CTPL_Num=0;
 int varible;
 int MyVaribale;
+    for(int i=0;i<n;i--){
+cout<<"Pankaj";
+        endl;
+        cout
+
+    }
     cout << "Hello World" << endl;
 }
