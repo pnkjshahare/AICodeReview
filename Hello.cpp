a7fb948793044af51b033bd4e21783a66fc81d93
Pankajshahare <110549683+pnkjshahare@users.noreply.github.com>
2025-10-27 15:52:49 +0530

    Correct function definitions and calculations
    Fix function signatures and add calculations.

--- Hello.cpp
+++ Hello.cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+int calculate(int num1,num2){
+return num2+num3;
+
+}
+int CTPLCal(int num1,num2){
+return num1+num2;
+
+
+}
 int main()
 {
 int varible;
 int MyVaribale;
+int CPTLCount=0;
     cout << "Hello World" << endl;
+    calculate(num1,num4);
+    CTPLCal(10,20);
 }
