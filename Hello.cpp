da51564cd2eb206993177c3b272a0fc666b7b3e5
Pankajshahare <110549683+pnkjshahare@users.noreply.github.com>
2025-10-28 10:55:15 +0530

    Add sum calculation function and update main
    Added a function to calculate the sum of two numbers and updated main to demonstrate its usage.

--- Hello.cpp
+++ Hello.cpp
@@ -1,13 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+//CalCulate Sum
+int CTPLCalculateSum(int num1,int num2){
+ int CTPLnum3=num1+num2;
+ return CTPLnum3;
+
+}
 // printing hello world
 void CTPLPrint(){
 cout<<"Hello World"<<endl;
 }
 int main()
 {
-int varible;
-int MyVaribale;
-cout << "Hello World" << endl;
- CTPLPrint();   
+ int varible;
+ int MyVaribale;
+  int CTPLnum1=10;
+  int CTPLnum2=20;
+  cout << "Hello World" << endl;
+  CTPLPrint();   
+  cout<<CTPLCalculateSum(CTPLnum1,CTPLnum2);
 }
