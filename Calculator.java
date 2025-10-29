2bbc17f16ae96225bece8b8efa2012b5742afff1
Pankajshahare <pankajsahare.04@gmail.com>
2025-10-29 16:07:54 +0530

    Adding java file for test case

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+
+    // Adds two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Returns maximum of two numbers
+    public static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+}
