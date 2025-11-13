ae968bf871d5fa56e8280d13002b07aedfb2c795
Pankajshahare <pankajsahare.04@gmail.com>
2025-11-13 16:17:54 +0530

    test

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
     public static int add(int a, int b) {
     	System.out.print("pankaj");
         return a + b+c;
+        return c+b+c;
     }
 
     // Returns maximum of two number
