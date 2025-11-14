6ab1d27cc2ff36e22ea0e78c5983c5026aa0c30d
Pankajshahare <pankajsahare.04@gmail.com>
2025-11-14 12:32:59 +0530

    test

--- Calculator.java
+++ Calculator.java
@@ -9,5 +9,13 @@ public class Calculator {
     }
 
     // Returns maximum of two number
+    
+    public int maxNo(int a,int b) {
+    	if(a>b) {
+    		return a;
+    	}
+    	else {
+    		return b;
+    	}
        
 }
