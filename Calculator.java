bfa0479f6d0fe54d6d04d40414ef1e18cfc38ce9
Pankajshahare <pankajsahare.04@gmail.com>
2025-11-20 11:52:12 +0530

    test

--- Calculator.java
+++ Calculator.java
@@ -25,3 +25,34 @@ public class Calculator {
 	   }
 	}
 
+
+
+
+public class Calculator {
+
+   public static void main(String []args) {
+	   System.out.print("Pankaj");
+	   
+   }
+}
+
+
+
+public class Calculator {
+
+   public static void main(String []args) {
+	   System.out.print("Pankaj");
+	   
+   }
+}
+
+public class Calculator {
+
+	   public static void main(String []args) {
+		   System.out.print("Pankaj");
+		   
+	   }
+	}
+
+
+
