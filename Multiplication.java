f2c8178a83a2625102140ad76700110bc45c212f
Pankajshahare <pankajsahare.04@gmail.com>
2025-11-20 14:56:31 +0530

    test

--- Multiplication.java
+++ Multiplication.java
@@ -16,3 +16,24 @@ public class Multiplication {
         System.out.println("Multiplication of three numbers: " + result);
     }
 }
+
+
+
+public class Multiplication {
+
+    // Function to multiply three numbers
+    public static int multiplyThreeNumbers(int a, int b, int c) {
+        return a * b * c;
+    }
+
+    // Main method to test the function
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        int num3 = 2;
+
+        int result = multiplyThreeNumbers(num1, num2, num3);
+        System.out.println("Multiplication of three numbers: " + result);
+    }
+}
+
