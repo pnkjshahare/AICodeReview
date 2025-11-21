9a7f3685e991d59d40054aefaa5c9f02cc8c8f85
Pankajshahare <pankajsahare.04@gmail.com>
2025-11-21 12:06:47 +0530

    test

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,5 @@
-OKDNIENV	ODDCSdpsSX
- sdsdcokdc oSDKF	WELFOSDC
- sd a[kmSDO]
-		 sdcj
-		 pankaj shahae
-		 wdo	wdjc sdsc
-		 sdsdmk	dhcpoe
-		 dslcl SDCd
-		 dlkcJHC
+
+public static void main() {
+	mlwdcck	w dcwepwed
+	wdckm	mdc	,	wdc
+}
