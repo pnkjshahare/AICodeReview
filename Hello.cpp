2fe55c7b8fed91152f51670da26c169878aac5ff
Pankajshahare <110549683+pnkjshahare@users.noreply.github.com>
2025-11-27 12:17:58 +0530

    Rename and update print function to Print
    Test

--- Hello.cpp
+++ Hello.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 // printing hello world
-void CTPLPrint(){
+void Print(){
 cout<<"Hello World"<<endl;
 }
 int main()
@@ -9,5 +9,8 @@ int main()
 int varible;
 int MyVaribale;
 cout << "Hello World" << endl;
- CTPLPrint();   
+ Print(); 
+  Print();
 }
+
+
