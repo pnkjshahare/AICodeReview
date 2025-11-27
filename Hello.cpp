34b2a64f57c0e0a1b0f4949a44bac76f34aee1bb
Pankajshahare <110549683+pnkjshahare@users.noreply.github.com>
2025-11-27 16:02:53 +0530

    test 2.0

--- Hello.cpp
+++ Hello.cpp
@@ -14,3 +14,19 @@ cout << "Hello World" << endl;
  cout << "Hello World" << endl;
  Print();   
 }
+#include <bits/stdc++.h>
+using namespace std;
+// printing hello world
+void Print(){
+cout<<"Hello World"<<endl;
+}
+int main()
+{
+int Name=0;
+cout << "Hello World" << endl;
+ Print();   
+ cout << "Hello World" << endl;
+ Print();   
+ cout << "Hello World" << endl;
+ Print();   
+}
