4c7cecc222f0c639b8e662a4df1464f6cd8ca330
Pankajshahare <110549683+pnkjshahare@users.noreply.github.com>
2025-12-02 17:38:16 +0530

    Add Print function to display 'Hello World'

--- Hello.cpp
+++ Hello.cpp
@@ -12,3 +12,18 @@ cout << "Hello World" << endl;
  Print();   
   Print();   
 }
+#include <bits/stdc++.h>
+using namespace std;
+// printing hello world
+void Print(){
+cout<<"Hello World"<<endl;
+}
+int main()
+{
+int varible;
+int MyVaribale;
+cout << "Hello World" << endl;
+ Print();   
+  Print();   
+}
+
